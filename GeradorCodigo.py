56f26c97ee3aaa49cfa5784b39c03a0e36ab29be
Gabriel <aguiarsantos25@gmail.com>
2017-12-05 09:58:34 -0200

    Transformed to objects @ js + python

--- GeradorCodigo.py
+++ GeradorCodigo.py
@@ -1,12 +1,18 @@
 from ANTLR.tileVisitor import *
 from ANTLR.tileParser import *
 
+class Tile():
+    nome = ''
+    acao = []
+
 class GeradorCodigo(tileVisitor):
 
     #Array que armazena as imagens
 
     imageArray = []
     imageCounter = 0
+    tileCounter = 0
+    acaoCounter = 1
 
     final = """<!DOCTYPE html><html><head><script type="text/javascript">var ctx = null;size = {size}
     var i = 0;
@@ -17,7 +23,11 @@ class GeradorCodigo(tileVisitor):
     }
     /*Tile Loading */
     var image = new Array();
+    var tiles = new Array();
+
     {tiles}
+
+    {images}
 
     var currentSecond = 0, frameCount = 0, framesLastSecond = 0;
     window.onload = function()
@@ -37,13 +47,23 @@ class GeradorCodigo(tileVisitor):
         self.final = self.final.replace('{tiles}',str(self.visitTile(ctx.tile())))
 
     def visitTile(self,ctx: tileParser.TileContext):
-        return "image["+str(self.imageCounter)+"].src = " +str(ctx.path().CADEIA()) + self.visitRecur_tiles(ctx.recur_tiles())
+        path = str(ctx.path().CADEIA())
+        name = str(ctx.ID())
+        tile = "tile["+str(self.imageCounter)+"] = { \n path:"+path+", name:'"+name+"', action :{"+self.visitAcao(ctx.acao(),self.imageCounter) + "}};" + "\nimage["+str(self.imageCounter)+"].src = tile["+str(self.imageCounter)+"].path;" + self.visitRecur_tiles(ctx.recur_tiles())
+
+        return tile;
+
+    def visitAcao(self,ctx: tileParser.AcaoContext,i):
+        return str(ctx.ID()) +": { path:" +str(ctx.path().CADEIA()) + "}," + self.visitRecur_acao(ctx.recur_acao(),self.imageCounter)
 
 
+    def visitRecur_acao(self, ctx: tileParser.Recur_acaoContext,i):
+        self.acaoCounter+=1
+        return "\n"+self.visitAcao(ctx.acao(),i) if ctx.acao() is not None else ''
 
     def visitRecur_tiles(self, ctx: tileParser.Recur_tilesContext):
         self.imageCounter+=1
-        return ";\n"+self.visitTile(ctx.tile()) if ctx.tile() is not None else ''
+        return "\n"+self.visitTile(ctx.tile()) if ctx.tile() is not None else ''
 
 
     def getCodigo(self):
